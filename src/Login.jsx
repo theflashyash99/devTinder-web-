722a37bd47c5b34ad2d643d67fee75a95023b59d
theflashyash99 <theflashyash99@gmail.com>
2026-01-09 13:30:45 +0530

    create Login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
-}
+  const [emailId, setEmailId] = useState();
+  const [password, setPassword] = useState();
 
-export default Login
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                className="input"
+                value={emailId}
+                placeholder=""
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                className="input"
+                value={password}
+                placeholder=""
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
