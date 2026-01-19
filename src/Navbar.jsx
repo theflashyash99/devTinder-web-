e8cb81c09ac2545e916cca3c09141af516c74b4a
theflashyash99 <theflashyash99@gmail.com>
2026-01-19 11:42:43 +0530

    RTK

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+
+  const user = useSelector(store => store.user);
   return (
     <div>
       <div className="navbar bg-base-300 shadow-sm">
