982bf65d3c243da5fd4def657dbb4f341e44e790
theflashyash99 <theflashyash99@gmail.com>
2026-01-29 12:03:59 +0530

    Feed request handle  page

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -1,9 +1,27 @@
+import axios from "axios";
 import React from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFeed } from "../utils/feedSlice";
+
 
 const UserCard = ({ user }) => {
-  console.log(user);
-  const { firstName, lastName, age, photoURL, about,gender } = user;
-  
+  const dispatch = useDispatch();
+  const { firstName, lastName, age, photoURL, about, gender,_id } = user;
+
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,
+        {},
+        { withCredentials: true },
+      );
+      dispatch(removeUserFeed(userId)); 
+      
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
     <div>
@@ -12,14 +30,14 @@ const UserCard = ({ user }) => {
           <img src={photoURL} alt="photo" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{firstName} {" "}  {lastName}</h2>
-          { age && gender && <p>{age + ", " + gender}</p>}
-          <p>
-           {about}
-          </p>
-          <div className="card-actions justify-end my-4">
-            <button className="btn btn-primary">Ignore</button>
-            <button className="btn btn-secondary">Interested</button>
+          <h2 className="card-title">
+            {firstName} {lastName}
+          </h2>
+          {age && gender && <p>{age + ", " + gender}</p>}
+          <p>{about}</p>
+          <div className="card-actions justify-end my-4 ">
+            <button className="btn btn-primary" onClick={()=>handleSendRequest("ignored",_id)}>Ignore</button>
+            <button className="btn btn-secondary" onClick={()=>handleSendRequest("interested",_id)}>Interested</button>
           </div>
         </div>
       </div>
